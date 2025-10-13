1d2eff1263413cb458bde5da8be86d46a773bc4c
Atif <atifatul752@gmail.com>
2025-10-13 13:18:51 +0530

    added updated part

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -213,14 +213,16 @@ const Contact = () => {
       </div>
       {/* // End Contact page */}
 
-      <div class="contact-map-section">
+      {/* Contact Map Section Start */}
+      <div className="contact-map-section">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.5647631857846!2d90.36311167605992!3d23.83407118555764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c14c8682a473%3A0xa6c74743d52adb88!2sEgens%20Lab!5e0!3m2!1sen!2sbd!4v1700138349574!5m2!1sen!2sbd"
-          allowfullscreen=""
+          src="https://www.google.com/maps?q=28.583228,77.315109&z=17&output=embed"
+          allowFullScreen=""
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
+      {/* Contact Map Section End*/}
     </>
   );
 };
